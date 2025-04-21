ca8bcd42d3166fce638bf89690a5e82392b7c6ff
Wayne Yang <b019020450@gmail.com>
2025-04-21 10:45:39 +0800

    Fill in Csharp sample code for Custom_models & Read & Layout

---
+++ .NET(v4.0)/CodeSnippetForApiVer_2024-11-30/Document analysis/Layout.cs
@@ -0,0 +1,92 @@
+﻿//#define RUN_AS_ENTRY_OF_TOP_LEVEL_STATEMENT
+#if RUN_AS_ENTRY_OF_TOP_LEVEL_STATEMENT
+
+/*
+  This code sample shows Prebuilt Layout operations with the Azure AI Document Intelligence client library. 
+
+  To learn more, please visit the documentation - Quickstart: Document Intelligence (formerly Form Recognizer) SDKs
+  https://learn.microsoft.com/azure/ai-services/document-intelligence/quickstarts/get-started-sdks-rest-api?pivots=programming-language-csharp
+*/
+
+using Azure;
+using Azure.AI.DocumentIntelligence;
+
+/*
+  Remember to remove the key from your code when you're done, and never post it publicly. For production, use
+  secure methods to store and access your credentials. For more information, see 
+  https://docs.microsoft.com/en-us/azure/cognitive-services/cognitive-services-security?tabs=command-line%2Ccsharp#environment-variables-and-application-configuration
+*/
+
+string endpoint = Environment.GetEnvironmentVariable("DOCUMENTINTELLIGENCE_ENDPOINT");
+string key = Environment.GetEnvironmentVariable("DOCUMENTINTELLIGENCE_API_KEY");
+
+AzureKeyCredential credential = new AzureKeyCredential(key);
+DocumentIntelligenceClient client = new DocumentIntelligenceClient(new Uri(endpoint), credential);
+
+// sample document
+Uri fileUri = new Uri("https://raw.githubusercontent.com/Azure-Samples/cognitive-services-REST-api-samples/master/curl/form-recognizer/sample-layout.pdf");
+
+Operation<AnalyzeResult> operation = await client.AnalyzeDocumentAsync(WaitUntil.Completed, "prebuilt-layout", fileUri);
+
+AnalyzeResult result = operation.Value;
+
+foreach (DocumentPage page in result.Pages)
+{
+    Console.WriteLine($"Document Page {page.PageNumber} has {page.Lines.Count} line(s), {page.Words.Count} word(s),");
+    Console.WriteLine($"and {page.SelectionMarks.Count} selection mark(s).");
+
+    for (int i = 0; i < page.Lines.Count; i++)
+    {
+        DocumentLine line = page.Lines[i];
+        Console.WriteLine($"  Line {i} has content: '{line.Content}'.");
+
+        Console.WriteLine($"    Its bounding box is:");
+        Console.WriteLine($"      Upper left => X: {line.Polygon[0]}, Y= {line.Polygon[1]}");
+        Console.WriteLine($"      Upper right => X: {line.Polygon[2]}, Y= {line.Polygon[3]}");
+        Console.WriteLine($"      Lower right => X: {line.Polygon[4]}, Y= {line.Polygon[5]}");
+        Console.WriteLine($"      Lower left => X: {line.Polygon[6]}, Y= {line.Polygon[7]}");
+    }
+
+    for (int i = 0; i < page.SelectionMarks.Count; i++)
+    {
+        DocumentSelectionMark selectionMark = page.SelectionMarks[i];
+
+        Console.WriteLine($"  Selection Mark {i} is {selectionMark.State}.");
+        Console.WriteLine($"    Its bounding box is:");
+        Console.WriteLine($"      Upper left => X: {selectionMark.Polygon[0]}, Y= {selectionMark.Polygon[1]}");
+        Console.WriteLine($"      Upper right => X: {selectionMark.Polygon[2]}, Y= {selectionMark.Polygon[3]}");
+        Console.WriteLine($"      Lower right => X: {selectionMark.Polygon[4]}, Y= {selectionMark.Polygon[5]}");
+        Console.WriteLine($"      Lower left => X: {selectionMark.Polygon[6]}, Y= {selectionMark.Polygon[7]}");
+    }
+}
+
+foreach (DocumentStyle style in result.Styles)
+{
+    // Check the style and style confidence to see if text is handwritten.
+    // Note that value '0.8' is used as an example.
+
+    bool isHandwritten = style.IsHandwritten.HasValue && style.IsHandwritten == true;
+
+    if (isHandwritten && style.Confidence > 0.8)
+    {
+        Console.WriteLine($"Handwritten content found:");
+
+        foreach (DocumentSpan span in style.Spans)
+        {
+            Console.WriteLine($"  Content: {result.Content.Substring(span.Offset, span.Length)}");
+        }
+    }
+}
+
+Console.WriteLine("The following tables were extracted:");
+
+for (int i = 0; i < result.Tables.Count; i++)
+{
+    DocumentTable table = result.Tables[i];
+    Console.WriteLine($"  Table {i} has {table.RowCount} rows and {table.ColumnCount} columns.");
+
+    foreach (DocumentTableCell cell in table.Cells)
+    {
+        Console.WriteLine($"    Cell ({cell.RowIndex}, {cell.ColumnIndex}) has kind '{cell.Kind}' and content: '{cell.Content}'.");
+    }
+}
